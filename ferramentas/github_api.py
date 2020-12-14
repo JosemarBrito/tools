57d5c40e01d1216f18209d99397cb51effb453c0
josemar <estudos.josemarbrito@gmail.com>
2020-12-14 01:17:45 -0300

    Configurado requests e criado arquivo de busca de avatar no github_api.py

---
+++ ferramentas/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Buscar o avatar de um usuário no Github
+
+    :param usuario: str com o nome do usuário no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+if __name__ == '__main__':
+    print(buscar_avatar('renzon'))
